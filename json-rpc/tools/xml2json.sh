519aa25c7defd46a55559c07eb8bb6c7325e78cb
Saeed Rasooli <saeed.gnu@gmail.com>
2023-10-28 10:02:02 +0330

    xml2json.sh: allow running from any directory

--- json-rpc/tools/xml2json.sh
+++ json-rpc/tools/xml2json.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+myPath=$(realpath "$0")
+myDir=$(dirname "$myPath")
+
+cd "$myDir"
+
 python xml2json.py ../handlers_E.xml E ..
 python xml2json.py ../handlers_D.xml D ..
 python xml2json.py ../handlers_C.xml C ..
